514ab15e7caa102aef350a1a3ca3b60bb4ff0ac2
Prakash Jat <prakashjatt966@gmail.com>
2026-07-26 16:55:20 +0530

    Fix auth flow bugs

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
+import enum
 from jose import jwt
 from jose.exceptions import JWTError
 import os
-from models.user import User
+from models.user import User, Role
 from dotenv import load_dotenv
 from passlib.context import CryptContext
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from fastapi.exceptions import HTTPException
 from db import get_db
 from fastapi import Depends, status
@@ -38,10 +39,10 @@ def verify_password(raw_password : str,hashed_password : str) -> bool:
 
 def create_token(user : User,type="access") -> str:
     jwt_exp = JWT_ACESS_TOKEN_EXPIRE if type == "access" else JWT_REFRESH_TOKEN_EXPIRE
-    expiry = datetime.now() + timedelta(minutes=int(jwt_exp))
+    expiry = datetime.now(timezone.utc) + timedelta(minutes=int(jwt_exp))
     payload = {
         "user_id" : user.id,
-        "user_role" : user.role,
+        "user_role" : user.role.value,
         "exp" : expiry,
         "type": type
     }
@@ -56,7 +57,7 @@ def verify_token(token : str,type="access") -> dict:
     except JWTError as e:
         raise HTTPException(status_code=400,detail=e)
     
-    if payload["exp"] < datetime.now():
+    if payload["exp"] < datetime.now().timestamp():
         raise HTTPException(status_code=400,detail={
             "msg" : "Token Expired"
         })
@@ -67,7 +68,7 @@ async def get_current_user(token : str = Depends(get_token),db : AsyncSession = 
     
     payload = verify_token(token)
     user_id = payload["user_id"]
-    user_role = payload["user_role"]               
+    user_role = Role(payload["user_role"])              
     result = await db.execute(select(User).where(User.id == user_id,User.role == user_role))
     user = result.scalar_one_or_none()
     if user is None:
@@ -86,7 +87,7 @@ def authorize(allowed_roles : list[str]):
     
     return checker
         
-def is_current_user(incoming_user: User, current_user : User = Depends(get_current_user)):
+def is_current_user(incoming_user: User, current_user : User):
     if incoming_user.id != current_user.id :
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="You are not an authorized person to acess this operation")
     
